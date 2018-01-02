acc93e597930f13ecc72ba37717c13aea3e1390c
Adham Khalilieh <Adham1011@github.com>
2018-01-02 02:16:29 +0200

    Dashboard

--- includes/main.js
+++ includes/main.js
@@ -1,4 +1,10 @@
-DashCalendar(){
+var days =["SUN","MON","TUE","WED","THU","FRI","SAT"];
+var months = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+function DashCalendar(){
+  var dashsection = document.getElementById("dashBoard");
   var date = new Date();
-  var month = date.getMonth();
+  dashsection.children[0].innerHTML=months[date.getMonth()];
+  console.log(months[date.getMonth()]);
 }
