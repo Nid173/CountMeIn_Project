c19a3b25acfc211a9f87e9379c442b2a94ed5264
Unknown <nid.stud@gmail.com>
2018-01-02 18:57:39 +0200

    updated Appointment page
    table++
    sidebar++ 
    LOADING PLus Jquery DIV

--- includes/main.js
+++ includes/main.js
@@ -1,4 +1,41 @@
 
+var appointmentsArr = new Array(); // empty array
+
+// object literal notation to create your structures
+appointmentsArr.push({ name1: 'nidal', purposeIndex: '1' });
+appointmentsArr.push({ name1: 'adham', purposeIndex: '0' });
+
+var purposeArr = new Array(); // empty array
+
+// object literal notation to create your structures
+purposeArr.push({ purposeName: 'Kill', time: '1Hour' });
+purposeArr.push({ purposeName: 'ghi', time: '30Minutes' });
+
+// PLUS MINUS
+
+
+
+
+function addTableRow(){
+	
+	for (var i = 0; i < appointmentsArr.length; i++) {
+  // use array[i] here 
+  var code2Append = 			`
+  								<tr>
+							     <th scope="row" ><div class="toggle"></div></th>
+							     <div class="content">
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+							     <td>`+appointmentsArr[i].name1+  `</td>
+							     <td>`+ purposeArr[ appointmentsArr[i].purposeIndex ].time +`</td>
+							     <td>`+ purposeArr[appointmentsArr[i].purposeIndex].purposeName +`</td>
+							     </tr>`;		
+	var mTbody = $('#tbody');
+	mTbody.append(code2Append);
+}
+	
+							
+};
+
 function ValidateDay(d){
   if(d-3 < 0){
     while (d>0) d--;
@@ -48,3 +85,13 @@ function DashCalendar(){
     dashsection.children[1].children[0].appendChild(elem);
   }
 };
+
+$(document).ready(function(){
+  var $content = $(".content").hide();
+  $(".toggle").on("click", function(e){
+    $(this).toggleClass("expanded");
+    $content.slideToggle();
+  });
+});
+
+
